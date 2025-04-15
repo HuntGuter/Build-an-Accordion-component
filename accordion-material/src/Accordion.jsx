bab2166a0c62fac95bddd2356d66ca249f594673
Nick Olshansky <huntguter@gmail.com>
2025-04-16 01:05:57 +0300

    first commit

---
+++ accordion-material/src/Accordion.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import {
+    Accordion as MuiAccordion,
+    AccordionSummary,
+    AccordionDetails,
+    Typography
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icon-material/ExpandMore";
+
+const Accordion = ({items}) => {
+    const [expandedIndex, setExpandedIndex] = useState(null);
+
+    const handleChange = (index) => (event, isExpanded) => {
+        setExpandedIndex(isExpanded ? index : null);
+    };
+
+    return (
+        <div>
+            {items.map((item, index) => (
+                <MuiAccordion
+                        key={index}
+                        expanded={expandedIndex === index}
+                        onChange={handleChange(index)}
+                >
+                    <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                        <Typography>{item.title}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>{item.content}</Typography>
+                    </AccordionDetails>
+                </MuiAccordion>
+            ))};
+        </div>
+    );
+};
+
+export default Accordion;
---
+++ accordion-material/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import Accordion from './Accordion';
+import { Container, Typography } from '@mui/material';
+
+const items = [
+  {
+    title: "What is Github and how does it work?",
+    content:
+      "GitHub is the home for all developers—a platform where you can share code, contribute to open source projects, or even automate your workflow with tools like GitHub Actions and Packages. If you’re just getting started with GitHub, you may know us best as a place for version control and collaboration.",
+  },
+  {
+    title: "How do I see GitHub's availability?",
+    content: "Check our real-time status report",
+  },
+  {
+    title: "Why is GitHub so popular?",
+    content:
+      "GitHub is built by developers for developers, and we’re proud to be home to the world’s largest open source community. With 50 million developers and millions more open source projects, GitHub has become the go-to place to collaborate and build software together.",
+  },
+]
+
+function App() {
+    return (
+      <Container maxWidth="md" sx={{mt: 5}}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          GitHub FAQ
+        </Typography>
+        <Accordion items={items}/>
+      </Container>
+    );
+}
+
+export default App
